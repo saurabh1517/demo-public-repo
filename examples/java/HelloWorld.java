d642d35d82f78583bdcafedbe3c446fd4d754490
Saurabh Jain <86323391+saurabh1517@users.noreply.github.com>
2025-09-15 12:27:51 -0700

    Add Java Hello World example

---
+++ examples/java/HelloWorld.java
@@ -0,0 +1,27 @@
+/**
+ * Hello World Example in Java
+ * 
+ * This is a simple Java program that demonstrates basic Java syntax
+ * and prints "Hello, World!" to the console.
+ * 
+ * Author: Saurabh Jain
+ * Date: 2025
+ */
+public class HelloWorld {
+    
+    /**
+     * Main method that prints Hello World message
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Java programming!");
+        
+        // Demonstrate some basic Java features
+        String name = "Saurabh";
+        System.out.println("Hello from " + name + "!");
+        
+        // Show different ways to print messages
+        System.out.println("This is a demo public repository showcasing various development projects.");
+    }
+}
