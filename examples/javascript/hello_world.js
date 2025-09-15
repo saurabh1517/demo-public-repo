9d391c720f1aedb58178aa87e224969dd5d795e4
Saurabh Jain <86323391+saurabh1517@users.noreply.github.com>
2025-09-15 12:27:48 -0700

    Add JavaScript Hello World example

---
+++ examples/javascript/hello_world.js
@@ -0,0 +1,25 @@
+/**
+ * Hello World Example in JavaScript
+ * 
+ * This is a simple JavaScript program that demonstrates basic JavaScript syntax
+ * and prints "Hello, World!" to the console.
+ * 
+ * Author: Saurabh Jain
+ * Date: 2025
+ */
+
+// Main function that prints Hello World message
+function main() {
+    console.log("Hello, World!");
+    console.log("Welcome to JavaScript programming!");
+    
+    // Demonstrate some basic JavaScript features
+    const name = "Saurabh";
+    console.log(`Hello from ${name}!`);
+    
+    // Show different ways to print messages
+    console.log("This is a demo public repository showcasing various development projects.");
+}
+
+// Call the main function
+main();
